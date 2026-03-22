b8cd0c41f7a3432f8578af60d666527ba970c579
LuluDiegs <luluca.diegues@gmail.com>
2026-03-22 18:36:31 -0300

    green: implementa Depositar

--- src/ContaBancaria/Conta.cs
+++ src/ContaBancaria/Conta.cs
@@ -44,8 +44,9 @@ public class Conta
     /// </summary>
     public void Depositar(decimal valor)
     {
-        // TODO: Implemente usando TDD
-        throw new NotImplementedException();
+        if (valor <= 0) throw new ArgumentException("O valor do depósito deve ser maior que zero.", nameof(valor));
+        if (!Ativa) throw new InvalidOperationException("Não é possível depositar em conta inativa.");
+        Saldo += valor;
     }
 
     /// <summary>
